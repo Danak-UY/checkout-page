d5b93f2f8583204ca435d61d80ceb7e1809e935d
Danak-UY <37196682+Danak-UY@users.noreply.github.com>
2023-08-14 19:22:59 -0300

    feat: custom checkbox

--- src/components/atoms/Checkbox.vue
+++ src/components/atoms/Checkbox.vue
@@ -7,14 +7,14 @@ defineProps<{
 </script>
 
 <template>
-  <span>
+  <span class="flex items-center">
     <input
       type="checkbox"
       :id="id"
-      class="font-semibold text-base p-4 rounded-xl border border-gray-neutral accent-orange-neutral scale-125 mr-2 text-white"
+      class="appearance-none mr-1 relative bg-white border-gray-icon checked:border-0 rounded-lg border-2 text-white h-8 w-8 scale-[60%] cursor-pointer transition-all inline-flex text-center justify-center items-center checked:bg-orange-neutral checked:after:border-b-[4px] checked:after:border-white checked:after:border-r-[4px] checked:after:rotate-45 after:-mt-1 checked:after:rounded-sm after:h-5 after:w-3 after:absolute after:text-xl after:text-white"
     />
 
-    <label :for="id" class="text-gray-neutral text-xs font-semibold mb-1.5">{{
+    <label :for="id" class="text-gray-neutral text-xs font-semibold">{{
       label
     }}</label>
   </span>
